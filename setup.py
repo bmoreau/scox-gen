3e5bfe2132f7cee33299c9b5160a92d82d9fb719
bmoreau <moreau.bertrand@gmail.com>
2016-01-31 14:28:08 +0100

    Project structure and first class diagram draft

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+      name='scox',
+      version='0.1',
+      description='NPC generator for INS-MV 4',
+      author='Bertrand Moreau',
+      url='https://github.com/bmoreau/scox-gen',
+      packages=['scox'],
+)
